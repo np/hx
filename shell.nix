37af0477c195c7c73689aa90130f077375dbb615
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2015-01-15 01:15:05 +0100

    [nix] shell.nix is now a function

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
-let pkgs = (import <nixpkgs> {});
-    haskellPackages = pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
-    extension = self : super :
-    let callPackage = self.callPackage;
-    in {
-       RFC1751 = callPackage ./nix/RFC1751.nix {};
-       thisPackage = haskellPackages.callPackage (import ./default.nix) {};
-    };});
+{ pkgs ? import <nixpkgs> {} }:
+let haskellPackages =
+      pkgs.recurseIntoAttrs
+        (pkgs.haskellPackages.override {
+           extension = self: super:
+                       {
+                         RFC1751 = self.callPackage ./nix/RFC1751.nix {};
+                         thisPackage = haskellPackages.callPackage (import ./default.nix) {};
+                       };
+         });
 in pkgs.lib.overrideDerivation haskellPackages.thisPackage (old: {
    buildInputs = old.buildInputs ++ [
      haskellPackages.cabalInstall
